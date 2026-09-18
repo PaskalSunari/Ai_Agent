ff700a7e538b537ef488bc3c95f5dfc06d1f4ba6
Paskal Sunari <paskalsunari@gmail.com>
2026-09-18 11:45:53 +0000

    calc

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello from aiagent!")
+import argparse
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
+load_dotenv()
+api_key = os.environ.get("OPENROUTER_API_KEY")
 
-if __name__ == "__main__":
-    main()
+if not api_key:
+    raise ValueError("OPENROUTER_API_KEY is missing from environment variables.")
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
+messages = [
+    {"role": "user", "content": args.user_prompt}
+]
+
+response = client.chat.completions.create(
+    model="openrouter/free",
+    messages=messages,
+)
+
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage.prompt_tokens}")
+    print(f"Response tokens: {response.usage.completion_tokens}")
+
+print("AI Response:", response.choices[0].message.content)
